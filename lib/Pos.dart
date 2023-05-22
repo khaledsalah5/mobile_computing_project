95b9c61e767b8358be35f7f27058c6ec434474d2
khaledsalah5 <ks1345@fayoum.edu.eg>
2023-05-22 23:54:08 +0300

    mob computing

--- lib/Pos.dart
+++ lib/Pos.dart
@@ -12,9 +12,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   List<LatLng> points = [];
-  //List<LatLng> points=[LatLng(29.317652, 30.846704 ),LatLng(29.312052, 30.846004 ),LatLng(29.307652, 30.846704 ),LatLng(29.317652, 30.846704)];
+  //List<LatLng> pointsTest =[LatLng(29.317652, 30.846704 ),LatLng(29.312052, 30.846004 ),LatLng(29.307652, 30.846704 ),LatLng(29.317652, 30.846704)];
 
   MapController mapController = MapController();
   late Stream<Position> positionStream;
@@ -24,15 +23,16 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     _getCurrentPosition();
 
-    positionStream = Geolocator.getPositionStream();
-    positionStream.listen((Position position) {
-      setState(() {
-        points.add(LatLng(position.latitude, position.longitude)); // Add location points
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      positionStream = Geolocator.getPositionStream();
+      positionStream.listen((Position position) {
+        setState(() {
+          points.add(LatLng(
+              position.latitude, position.longitude)); // Add location points
+        });
       });
+      print(points);
     });
-
-    print(points);
-   // _getStreamPosition();
   }
 
   Position? _currentPosition;
@@ -64,59 +64,35 @@ class _MyHomePageState extends State<MyHomePage> {
     await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
         .then((Position position) {
       setState(() => _currentPosition = position);
-      //_getAddressFromLatLng(_currentPosition!);
     }).catchError((e) {
       debugPrint(e);
     });
   }
 
-  Future<void> _getStreamPosition() async {
-    final LocationSettings locationSettings = LocationSettings(
-      accuracy: LocationAccuracy.high,
-      distanceFilter: 100,
-    );
-    StreamSubscription<Position> positionStream = Geolocator.getPositionStream(
-        locationSettings: locationSettings).listen(
-            (Position? position) {
-              print('streammm');
-          print(position == null ? 'Unknown' : '${position.latitude
-              .toString()}, ${position.longitude.toString()}');
-
-
-
-        });
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Offline Map For Fayoum", style: TextStyle(fontWeight: FontWeight.w500),),          actions: [
-        IconButton(
-            onPressed: () {
-              //_StreamPosition();
-              setState(() {
-              });
-              //print(points1.last);
-              //_MyHomePageState() => _MyHomePageState();
-            },
-            tooltip: 'Start Tracking',
-            icon: Icon(Icons.track_changes_outlined)),
-        IconButton(
-            onPressed: () {
-              setState(() {
-                points.clear();
-              });
-              // print(points.last);
-            },
-            tooltip: 'Reset your route',
-            icon: Icon(Icons.settings_backup_restore_rounded)),
-      ],),
+      appBar: AppBar(
+        title: const Text(
+          "Offline Map For Fayoum",
+          style: TextStyle(fontWeight: FontWeight.w500),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                  points.clear();
+              },
+              tooltip: 'Reset your route',
+              icon: const Icon(Icons.settings_backup_restore_rounded)),
+        ],
+      ),
       body: SafeArea(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('LAT: ${_currentPosition?.latitude ?? ""}'),
-              Text('LNG: ${_currentPosition?.longitude ?? ""}'),
+              Text('Latitude: ${_currentPosition?.latitude ?? ""}'),
+              Text('Longitude: ${_currentPosition?.longitude ?? ""}'),
               Flexible(
                 child: FlutterMap(
                   options: MapOptions(
@@ -135,20 +111,22 @@ class _MyHomePageState extends State<MyHomePage> {
                     MarkerLayer(
                       markers: [
                         Marker(
-                          width: 15.0,
-                          height: 15.0,
-                          point: LatLng(_currentPosition?.latitude ?? 0.0,_currentPosition?.longitude ?? 0.0),
-                          //point: LatLng(29.3195233, 30.8330717),
-                          builder: (ctx) =>const Icon(Icons.pix_outlined, color: Color.fromARGB(255, 13, 146, 146)),
+                          width: 10,
+                          height: 10,
+                          point: LatLng(
+                              _currentPosition?.latitude ?? 29.3195233,
+                              _currentPosition?.longitude ?? 30.8330717),
+                          builder: (ctx) => const Icon(Icons.arrow_drop_down_circle,
+                              color: Color.fromARGB(255, 13, 146, 146)),
                         ),
                       ],
                     ),
                     PolylineLayer(
                       polylines: [
                         Polyline(
-                          points: points, // Set the points of the polyline to the user's route points
+                          points: points,
                           color: Colors.blue,
-                          strokeWidth: 3,
+                          strokeWidth: 5,
                           strokeCap: StrokeCap.round,
                           strokeJoin: StrokeJoin.round,
                           isDotted: true,
@@ -159,19 +137,17 @@ class _MyHomePageState extends State<MyHomePage> {
                   ],
                 ),
               ),
-              //Text('ADDRESS: ${_currentAddress ?? ""}'),
               const SizedBox(height: 32),
-
             ],
           ),
         ),
       ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: _getCurrentPosition,
-      child: Icon(
-        Icons.my_location_rounded,
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getCurrentPosition,
+        child: const Icon(
+          Icons.my_location_rounded,
+        ),
       ),
-    ),
     );
   }
 }
